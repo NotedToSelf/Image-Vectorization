d7be813998a0fac69ddaa92484cf18015e4938cf
Erik Bogeberg <bogeberg@ultron.cs.pdx.edu>
2018-05-15 11:27:37 -0700

    fixed a bug in get_subpixel

--- ncuts.py
+++ ncuts.py
@@ -7,16 +7,15 @@ from skimage import data, segmentation, color, io, measure
 #Create Subpixel Edge Image
 #Returns edge map with edges marked with 1, junction points marked with 2  
 def get_subpixel(label):
-	height, width = label.shape
-	edges = np.zeros((height*2-1, width*2-1))
-	height, width = edges.shape
-
+	width, height = label.shape
+	edges = np.zeros((width*2-1, height*2-1))
+	width, height = edges.shape
 	#determine edges from label image
 	for x in range(width):
 		for y in range(height):
-			if ( ((x%2 != 0) and (y%2 == 0)) and label[int((x+1)/2)][int(y/2)] != label[int((x-1)/2)][int(y/2)] ):
+			if ( ((x%2 != 0) and (y%2 == 0)) and label[(x+1)//2][y//2] != label[(x-1)//2][y//2] ):
 				edges[x][y] = 1
-			if ( ((x%2 == 0) and (y%2 != 0)) and label[int(x/2)][int((y+1)/2)] != label[int(x/2)][int((y-1)/2)] ):
+			if ( ((x%2 == 0) and (y%2 != 0)) and label[x//2][(y+1)//2] != label[x//2][(y-1)//2] ):
 				edges[x][y] = 1
 
 	#Fill the gaps between edge pixels
@@ -41,7 +40,6 @@ def get_subpixel(label):
 infile = './images/bird.jpg'
 image = io.imread(infile)
 height, width, depth = image.shape
-
 #maybe tune these parameters to image size or density??
 #Detail fine equation: 
 #Detail med equation:
